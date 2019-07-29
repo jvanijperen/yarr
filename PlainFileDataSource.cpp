36012aaef229e0ea348e7aa8d940c0dd28e1eeab
Jeroen van IJperen <j.vanijperen@gmail.com>
2019-07-29 11:24:48 +0200

    Changed max line length from literal value to named constant

--- PlainFileDataSource.cpp
+++ PlainFileDataSource.cpp
@@ -3,10 +3,11 @@
 PlainFileDataSource::PlainFileDataSource(std::istream &in)
 : _dataEntryList()
 {
+	static const int maxLineLength = 1024;
 	while (in.good()) {
 		LineEntry entry;
-		char lineBuf[1024];
-		in.getline(lineBuf, 1024);
+		char lineBuf[maxLineLength];
+		in.getline(lineBuf, maxLineLength);
 		entry.line = lineBuf;
 		if (entry.line.size() > 0) {
 			_dataEntryList.push_back(entry);
